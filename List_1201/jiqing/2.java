d0cdad4c56b8f5907e8f178418fa2f3508b7d140
uprightclear <uprightclear@gmail.com>
2021-12-02 10:59:01 -0800

    20211202 update

--- List_1201/jiqing/2.java
+++ List_1201/jiqing/2.java
@@ -8,6 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+//this question is easy, no comment!
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode ans = new ListNode(0);
