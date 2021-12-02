075d70e52efb860abe6245c96f1f8d2c525747f1
uprightclear <uprightclear@gmail.com>
2021-12-02 14:45:25 -0800

    20211202 update

---
+++ List_1201/jiqing/21.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode ans = new ListNode(0);
+        ListNode tail = ans;
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        if(l1 != null) tail.next = l1;
+        if(l2 != null) tail.next = l2;
+        return ans.next;
+    }
+}
